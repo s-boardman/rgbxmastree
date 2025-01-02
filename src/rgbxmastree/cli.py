e768182910fd8696a147f16a042e87ea454d331f
Simon Boardman <simonboardman@gmail.com>
2025-01-02 22:14:44 +0000

    Setup --version option at the top level

--- src/rgbxmastree/cli.py
+++ src/rgbxmastree/cli.py
@@ -1,5 +1,7 @@
 import sys
+from typing import Optional
 from gpiozero.exc import NativePinFactoryFallback, PinFactoryFallback
+from typing_extensions import Annotated
 import typer
 
 from .commands.color_shuffle import app as color_shuffle_app
@@ -12,6 +14,28 @@ if not sys.warnoptions:
     warnings.filterwarnings("ignore", category=PinFactoryFallback)
     warnings.filterwarnings("ignore", category=NativePinFactoryFallback)
 
+
 app = typer.Typer()
+
+
+def _version_callback(version):
+    if version:
+        from rgbxmastree import __version__
+
+        print(f"rgbxmastree version: {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def version(
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version", callback=_version_callback, help="Show version and exit."
+        ),
+    ] = None,
+):
+    pass
+
 
 app.add_typer(color_shuffle_app)
