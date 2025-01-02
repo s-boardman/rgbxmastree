142e3ac992edc5f3a5ac6d8fe9facfd738daa516
Simon Boardman <simonboardman@gmail.com>
2025-01-02 21:33:06 +0000

    Remove NativePinFactoryFallback and PinFactoryFallback warnings from cli

--- src/rgbxmastree/cli.py
+++ src/rgbxmastree/cli.py
@@ -1,6 +1,16 @@
+import sys
+from gpiozero.exc import NativePinFactoryFallback, PinFactoryFallback
 import typer
 
 from .commands.color_shuffle import app as color_shuffle_app
+
+
+# Setup warning filtering
+if not sys.warnoptions:
+    import warnings
+
+    warnings.filterwarnings("ignore", category=PinFactoryFallback)
+    warnings.filterwarnings("ignore", category=NativePinFactoryFallback)
 
 app = typer.Typer()
 
